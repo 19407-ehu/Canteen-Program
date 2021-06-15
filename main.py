168a7b46d1d94d42736e991b1023152dda3e976a
19407 <80858530+19407-ehu@users.noreply.github.com>
2021-06-15 02:50:43 +0000

    sys import added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 #Fraser High Canteen Assessment - Harley Lambert-Ehu
 
 #Sequence 1: a normal te reo greeting
@@ -8,23 +10,21 @@ print("Nice to meet ya, " + name)
 
 #Sequence 2: 
 
-program_continue = print("Cool! So here's what we've got")
-
-end_program = print("Ok then. Have a nice day.")
-
-print(input("would you like to see the menu? "))
+input("would you like to see the menu? ")
 answer = input().lower()
 if answer == "yes" or answer == "y":
   answer = "yes"
-  print(program_continue)
+  print("Cool! Here's what we have.")
 
 elif answer == "no" or answer == "no":
   answer = "no"
-  print(end_program)
-  exit
+  print("Ok then, have a nice day!")
+  sys.exit()
 
 else:
   print("Sorry, was that a yes or no?")
+
+
 
 
 
